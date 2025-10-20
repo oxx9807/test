9a98af8cf5d2a7bae525559e18a9ca7f8d5a1805
oxx9807 <139927564+oxx9807@users.noreply.github.com>
2025-10-20 04:44:27 +0300

    Update setup_moneroocean_miner.sh

--- setup_moneroocean_miner.sh
+++ setup_moneroocean_miner.sh
@@ -220,7 +220,6 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/moneroocean/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/moneroocean/config.json
